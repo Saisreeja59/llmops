2d5cc04136bd4a3d4eaecabc31f9cc771c80ade3
Saisreeja59 <703435961@genpact.com>
2026-04-09 19:00:16 +0530

    Create test_basic.py

---
+++ tests/test_basic.py
@@ -0,0 +1,11 @@
+from app.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_app_exists():
+    assert app is not None
+
+def test_health():
+    response = client.get("/health/")
+    assert response.status_code == 200
